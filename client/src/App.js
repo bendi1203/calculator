16640ed8d8db3b513d8c658c88187aad054c5bf5
boss1203 <h983246@stud.u-szeged.hu>
2023-06-13 17:53:56 +0200

    front-end basics

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,34 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-        <h1>Hello world!</h1>
+    <div className="calculator">
+
+        <input type="text" className="input"></input>
+        
+        <div className="buttons">
+            <button type="button" className="btn" value="+">+</button>
+            <button type="button" className="btn" value="-">-</button>
+            <button type="button" className="btn" value="*">*</button>
+            <button type="button" className="btn" value="/">/</button>
+
+            <button type="button" className="btn" value="7">7</button>
+            <button type="button" className="btn" value="8">8</button>
+            <button type="button" className="btn" value="9">9</button>
+            
+            <button type="button" className="btn" value="4">4</button>
+            <button type="button" className="btn" value="5">5</button>
+            <button type="button" className="btn" value="6">6</button>
+
+            <button type="button" className="btn" value="1">1</button>
+            <button type="button" className="btn" value="2">2</button>
+            <button type="button" className="btn" value="3">3</button>
+
+            <button type="button" className="btn" value="0">0</button>
+            <button type="button" className="btn" value=".">.</button>
+            <button type="button" className="btn" value="AC">AC</button>
+
+            <button type="button" className="btn equal" value="=">=</button>
+        </div>
     </div>
   );
 }
