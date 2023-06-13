63684befe7e722cc0c73c1a669dd1c6219e843d1
boss1203 <h983246@stud.u-szeged.hu>
2023-06-13 18:57:58 +0200

    save endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -7,26 +7,26 @@ function App() {
         <input type="text" className="input"></input>
         
         <div className="buttons">
-            <button type="button" className="btn" value="+">+</button>
-            <button type="button" className="btn" value="-">-</button>
-            <button type="button" className="btn" value="*">*</button>
-            <button type="button" className="btn" value="/">/</button>
+            <button type="button" value="+">+</button>
+            <button type="button" value="-">-</button>
+            <button type="button" value="*">*</button>
+            <button type="button" value="/">/</button>
 
-            <button type="button" className="btn" value="7">7</button>
-            <button type="button" className="btn" value="8">8</button>
-            <button type="button" className="btn" value="9">9</button>
+            <button type="button" value="7">7</button>
+            <button type="button" value="8">8</button>
+            <button type="button" value="9">9</button>
             
-            <button type="button" className="btn" value="4">4</button>
-            <button type="button" className="btn" value="5">5</button>
-            <button type="button" className="btn" value="6">6</button>
+            <button type="button" value="4">4</button>
+            <button type="button" value="5">5</button>
+            <button type="button" value="6">6</button>
 
-            <button type="button" className="btn" value="1">1</button>
-            <button type="button" className="btn" value="2">2</button>
-            <button type="button" className="btn" value="3">3</button>
+            <button type="button" value="1">1</button>
+            <button type="button" value="2">2</button>
+            <button type="button" value="3">3</button>
 
-            <button type="button" className="btn" value="0">0</button>
-            <button type="button" className="btn" value=".">.</button>
-            <button type="button" className="btn" value="AC">AC</button>
+            <button type="button" value="0">0</button>
+            <button type="button" value=".">.</button>
+            <button type="button" value="AC">AC</button>
 
             <button type="button" className="btn equal" value="=">=</button>
         </div>
--- server/index.js
+++ server/index.js
@@ -2,9 +2,36 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+path = require('path')
+fs = require('fs');
+url = require('url');
+
+app.use(express.json())
+
+app.post('/save', (req, res) => {
+  const data = req.body.value;
+  const filePath = path.join(__dirname, 'data', 'data.txt');
+
+  if(typeof data === 'number') {
+    fs.appendFile(filePath, JSON.stringify(data) + '\n', (err) => {
+      if(err) {
+        console.log(err);
+        res.status(500).send('Cannot save the data!');
+      } else {
+        res.status(200).send('Data saved successfully!');
+      }
+    })
+  } else {
+    res.status(500).send('Only numbers can be saved!');
+  }
+
+  
 })
+
+app.get('/load', (req, res) => {
+  res.send('load')
+})
+
 
 app.listen(port, () => {
   console.log(`Calculator app listening on port ${port}`)
