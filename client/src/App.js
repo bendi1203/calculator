e99bb6364b4f8707a996cddc8c018f393ad35fab
boss1203 <h983246@stud.u-szeged.hu>
2023-06-15 13:05:49 +0200

    frontend styling

--- client/src/App.js
+++ client/src/App.js
@@ -7,28 +7,29 @@ function App() {
         <input type="text" className="input"></input>
         
         <div className="buttons">
-            <button type="button" value="+">+</button>
-            <button type="button" value="-">-</button>
-            <button type="button" value="*">*</button>
-            <button type="button" value="/">/</button>
+            <button type="button" value="AC" className="operation ac">AC</button>
+            <button type="button" value="MC" className="operation mc">MC</button>
+            <button type="button" value="MS" className="operation ms">MS</button>
+            <button type="button" value="/" className="operation div">÷</button>
 
-            <button type="button" value="7">7</button>
-            <button type="button" value="8">8</button>
-            <button type="button" value="9">9</button>
+            <button type="button" value="7" className="number n7">7</button>
+            <button type="button" value="8" className="number n8">8</button>
+            <button type="button" value="9" className="number n9">9</button>
+            <button type="button" value="*" className="operation mul">×</button>
             
-            <button type="button" value="4">4</button>
-            <button type="button" value="5">5</button>
-            <button type="button" value="6">6</button>
+            <button type="button" value="4" className="number n4">4</button>
+            <button type="button" value="5" className="number n5">5</button>
+            <button type="button" value="6" className="number n6">6</button>
+            <button type="button" value="-" className="operation minus">-</button>
 
-            <button type="button" value="1">1</button>
-            <button type="button" value="2">2</button>
-            <button type="button" value="3">3</button>
-
-            <button type="button" value="0">0</button>
-            <button type="button" value=".">.</button>
-            <button type="button" value="AC">AC</button>
-
-            <button type="button" className="btn equal" value="=">=</button>
+            <button type="button" value="1" className="number n1">1</button>
+            <button type="button" value="2" className="number n2">2</button>
+            <button type="button" value="3" className="number n3">3</button>
+            <button type="button" value="+" className="operation plus">+</button>
+            
+            <button type="button" value="0" className="number n0">0</button>
+            <button type="button" value="." className="number dot">.</button>
+            <button type="button" value="=" className="equal">=</button>
         </div>
     </div>
   );
